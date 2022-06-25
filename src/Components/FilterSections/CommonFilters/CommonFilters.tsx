9abfc0b7b806ba6544e7205e9546b3a62ac16e6f
Gagarin-One <92833239+Gagarin-One@users.noreply.github.com>
2022-06-25 06:49:10 +0300

    transferring the range filter from the local state to the editor

--- src/Components/FilterSections/CommonFilters/CommonFilters.tsx
+++ src/Components/FilterSections/CommonFilters/CommonFilters.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { FC, useEffect, useState } from "react";
 import s from './CommonFilters.module.scss'
 import Select, { OnChangeValue } from 'react-select'
 import Slider from 'rc-slider';
@@ -8,16 +8,19 @@ import 'rc-slider/assets/index.css';
 import { useAppSelector, useAppDispatch } from '../../../Store/hooks';
 import {MainSlice} from '../../../Store/Reducers/AppSlice';
 import FilterSections from "../FiltersSections";
+import { FetchAds } from "../../../Store/Reducers/actionCreators";
+import VButton from "../../Etc/ViewButton/vButton";
 
+type CommonFiltersType = {createArr:any}
 
-
-const CommonFilters = () => {
-
+const CommonFilters:FC<CommonFiltersType> = ({createArr}) => {
+  const {priceRange} = useAppSelector(state=> state.mainReducer)
   const {category} = useAppSelector(state=> state.mainReducer)
   const {changeCategory} = MainSlice.actions
+  const {setRange} = MainSlice.actions
+
   const dispatch = useAppDispatch()
 
-  let [range, setRange] = useState([0,1000])
 
   type Option = {value: string, label: string}
 
@@ -34,7 +37,7 @@ const CommonFilters = () => {
   }
 
   let onChangeRange = (value:any) => {
-    setRange(value)
+    dispatch(setRange(value));
   }
   
   return (
@@ -44,8 +47,8 @@ const CommonFilters = () => {
       <Select className={s.select} onChange={onHandleChange} options={options} defaultValue={options[0]}/>
       <p className={s.price}>Цена, ₽ </p>
       <div className={s.sumPrice} >
-        <div>от {range[0]}</div>
-        <div>до {range[1]}</div>
+        <div>от {priceRange[0]}</div>
+        <div>до {priceRange[1]}</div>
       </div>
       <div className={s.slider}>
         <Slider
@@ -57,6 +60,9 @@ const CommonFilters = () => {
         />
       </div>
       <FilterSections CategoryValue={category?.value }/>
+      <div style={{marginLeft:'109px'}}>
+        <VButton createArr={createArr}/>
+      </div>
     </div>
   )
 }
