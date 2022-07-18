981eb3a1ee629fe9cee3a19a1e87e86f52a0e7a3
Gagarin-One <92833239+Gagarin-One@users.noreply.github.com>
2022-07-18 21:04:44 +0900

    prepare input in cameras filter

--- src/Components/FilterSections/CategoryOfFilters/CamerasFilter/CamerasFilter.tsx
+++ src/Components/FilterSections/CategoryOfFilters/CamerasFilter/CamerasFilter.tsx
@@ -2,12 +2,23 @@ import React from "react";
 import VButton from "../../../Etc/ViewButton/ResultButton";
 import s from "./CamerasFilter.module.scss"
 import Select, { OnChangeValue } from 'react-select'
+import { useAppDispatch, useAppSelector } from "../../../../Store/hooks";
+import { MainSlice } from "../../../../Store/Reducers/AppSlice";
 
 const CamerasFilter = ( ) => {
 
   type Option = {value: string, label: string}
-  let onHandleChange = (selectedOption : OnChangeValue<Option, false>) => {
-    
+  const dispatch = useAppDispatch()
+  const {changeDopeFilterSelect} = MainSlice.actions
+  const {changeFilterSelect} = MainSlice.actions
+  const {filterData} = useAppSelector(state => state.mainReducer)
+  const {changeFilterCheckbox} = MainSlice.actions
+  const {removeFilterCheckbox} = MainSlice.actions
+  const onHandleChange = (selectedOption : OnChangeValue<Option, false>) => {
+    dispatch(changeFilterSelect(selectedOption!.value)) 
+  }
+  const DopeOnHandleChange = (selectedOption : OnChangeValue<Option, false>) => {
+    dispatch(changeDopeFilterSelect(selectedOption!.value)) 
   }
   const matrixOptions = [
     { value: '27.9×8.16', label: '27.9×8.16' },
@@ -20,19 +31,25 @@ const CamerasFilter = ( ) => {
     { value: '1280×720', label: '1280×720' },
     { value: '1920×1080', label: '1920×1080' },
   ]
+  const checkboxChange = (checkbox:string) => {     //можно вынести
+    filterData.checkbox && 
+    filterData.checkbox.some((item:string) => item === checkbox) ?
+    dispatch(removeFilterCheckbox(checkbox)):
+    dispatch(changeFilterCheckbox(checkbox))
+  }
   return (
     <div className={s.wrapper}>
       <p className={s.title}>Тип фотоаппарата</p>
       <div className={s.checkbox}>
-        <input type='checkbox' className={s.myinput}/>
+        <input type='checkbox' onChange={() =>checkboxChange('mirrored')} className={s.myinput}/>
         <p>Зеркальный</p>
       </div>
       <div className={s.checkbox}>
-        <input type='checkbox' className={s.myinput}/>
+        <input type='checkbox' onChange={() =>checkboxChange('digital')} className={s.myinput}/>
         <p>Цифровой</p>
       </div>
       <div className={s.checkbox}>
-        <input type='checkbox' className={s.myinput}/>
+        <input type='checkbox' onChange={() =>checkboxChange('mirrorless')} className={s.myinput}/>
         <p>Беззеркальный</p>
       </div>
       <p className={s.title}>Минимальное разрешение матрицы</p>
@@ -45,7 +62,7 @@ const CamerasFilter = ( ) => {
       <p className={s.title}>Минимальное разрешение видео</p>
       <Select 
         className={s.select}
-        onChange={onHandleChange}
+        onChange={DopeOnHandleChange}
         options={resolutionOptions}
         defaultValue={resolutionOptions[0]}
       />
