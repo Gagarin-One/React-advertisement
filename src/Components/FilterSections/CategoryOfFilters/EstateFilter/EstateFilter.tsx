78e73209798ae87124ca56d8ad7d57f93ce612ed
Gagarin-One <92833239+Gagarin-One@users.noreply.github.com>
2022-07-18 21:06:34 +0900

    prepare input in estate filter

--- src/Components/FilterSections/CategoryOfFilters/EstateFilter/EstateFilter.tsx
+++ src/Components/FilterSections/CategoryOfFilters/EstateFilter/EstateFilter.tsx
@@ -2,11 +2,19 @@ import React from "react";
 import s from './EstateFilter.module.scss'
 import Select, { OnChangeValue } from 'react-select'
 import VButton from "../../../Etc/ViewButton/ResultButton";
+import { useAppDispatch, useAppSelector } from "../../../../Store/hooks";
+import { MainSlice } from "../../../../Store/Reducers/AppSlice";
 
 const EstateFilter = () => {
   type Option = {value: string, label: string}
+  const dispatch = useAppDispatch()
+  const {changeFilterSwitch} = MainSlice.actions
+  const {changeFilterSelect} = MainSlice.actions
+  const {filterData} = useAppSelector(state => state.mainReducer)
+  const {changeFilterCheckbox} = MainSlice.actions
+  const {removeFilterCheckbox} = MainSlice.actions
   let onHandleChange = (selectedOption : OnChangeValue<Option, false>) => {
-    
+    dispatch(changeFilterSelect(selectedOption!.value)) 
   }
   const options = [
     { value: '1990', label: '1990' },
@@ -15,19 +23,31 @@ const EstateFilter = () => {
     { value: '2010', label: '2010' },
     { value: '2020', label: '2020' }
   ]
+
+  const onRoomsChange = (rooms:string) => {
+    dispatch(changeFilterSwitch(rooms))
+  }
+
+  const checkboxChange = (checkbox:string) => {     //можно вынести
+    filterData.checkbox && 
+    filterData.checkbox.some((item:string) => item === checkbox) ?
+    dispatch(removeFilterCheckbox(checkbox)):
+    dispatch(changeFilterCheckbox(checkbox))
+  }
+
   return (
     <div className={s.wrapper}>
       <p className={s.title}>Тип недвижимости</p>
       <div className={s.checkbox}>
-        <input type='checkbox' className={s.myinput}/>
+        <input type='checkbox' onChange={() =>checkboxChange('house')} className={s.myinput}/>
         <p>Дом</p>
       </div>
       <div className={s.checkbox}>
-        <input type='checkbox' className={s.myinput}/>
+        <input type='checkbox' onChange={() =>checkboxChange('flat')} className={s.myinput}/>
         <p>Квартира</p>
       </div>
       <div className={s.checkbox}>
-        <input type='checkbox' className={s.myinput}/>
+        <input type='checkbox' onChange={() =>checkboxChange('apartment')} className={s.myinput}/>
         <p>Апартаменты</p>
       </div>
       <p className={s.title}>Минимальная площадь,м²</p>
@@ -39,12 +59,12 @@ const EstateFilter = () => {
       />
       <p className={s.title}>Колличество комнат</p>
       <div className={s.rooms}>
-        <p>Любое</p>
-        <p>1</p>
-        <p>2</p>
-        <p>3</p>
-        <p>4</p>
-        <p>5+</p>
+        <button onClick={() => onRoomsChange('any')}>Любое</button>
+        <button onClick={() => onRoomsChange('1')}>1</button>
+        <button onClick={() => onRoomsChange('2')}>2</button>
+        <button onClick={() => onRoomsChange('3')}>3</button>
+        <button onClick={() => onRoomsChange('4')}>4</button>
+        <button onClick={() => onRoomsChange('5+')}>5+</button>
       </div>
     </div>
   )
