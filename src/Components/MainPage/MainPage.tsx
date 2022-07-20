93289352cab28094f535be68d7d16b288dac0165
Gagarin-One <92833239+Gagarin-One@users.noreply.github.com>
2022-07-20 23:29:06 +0900

    preliminary version of the filter

--- src/Components/MainPage/MainPage.tsx
+++ src/Components/MainPage/MainPage.tsx
@@ -1,55 +1,77 @@
 import React, { useEffect, useState } from "react";
-import { useAppDispatch, useAppSelector } from "../../Store/hooks";
-import { FetchAds } from "../../Store/Reducers/actionCreators";
+import { useAppSelector, useAppDispatch } from '../../Store/hooks';
 import Content from "../ContentComponents/Content";
 import CommonFilters from "../FilterSections/CommonFilters/CommonFilters";
 import ResultButton from "../Etc/ViewButton/ResultButton"
-import ItemSlider from "../ItemSlider/ItemSlider";
 import s from './MainPage.module.scss'
+import { MainSlice } from "../../Store/Reducers/AppSlice";
 const MainPage = ( ) => {
-  const dispatch = useAppDispatch()
-  const {ads} = useAppSelector(state => state.mainReducer) 
   const {category} = useAppSelector(state=> state.mainReducer)
+  const {ads} = useAppSelector(state => state.mainReducer) // content items 
   const {sliderRange} = useAppSelector(state=> state.mainReducer)
   const {filterData} = useAppSelector(state=> state.mainReducer)
-  let [optionalFilter, setOptFilter] = useState([])
-  let [priorityFilter, setPriFilter] = useState([])
-  let [filteredArr, setFilteredArr] = useState<any>([])  //initial!
-    
-  
+  const dispatch = useAppDispatch()
+
+  let [filteredArr, setFilteredArr] = useState<any>([])  
   useEffect(() => {
-    dispatch(FetchAds('Ads'))
-  },[])
+    setFilteredArr([])
+  },[category])
 
-  // const filterArr = (filter:any) => {                     // add type
-  //   if (optionalFilter.some(f => f === filter)) {
-  //     for (let i = 0; i < optionalFilter.length; i++) {  // need refactoring
-  //       if(optionalFilter[i] === filter) {
-  //         setOptFilter(optionalFilter.splice(i,1))
-  //       }
-  //     } 
-  //   } else { setOptFilter(filter) }
-  // }
-
+            // main filter
   const createFilteredArr = () => {
-    console.log(filterData)
-    console.log(sliderRange)
-    console.log(filterData.checkbox)
-    // let filt = ads
-    // // filt.filter(item => item.price < priceRange[0] && item.price > priceRange[1])
-    // for (let i = 0 ; i < filt.length; i++){
-    //   if (filt[i].price > priceRange[0] && filt[i].price < priceRange[1]) {
-    //     filt.push(filt[i])
-    //   } 
-    // }
-    // console.log(filteredArr)
+    let localArray = []
+    
+    if(filterData.select[0]){
+      for (let i = 0 ; i < ads.length; i++){
+        if (ads[i].tags.select >= filterData.select[0]){
+          localArray.push(ads[i])
+        }
+      }
+    }
+
+    if (filterData.switch[0]){
+      if (filterData.switch[0] !== 'any'){
+        for (let i = 0 ; i < ads.length; i++){
+          localArray = localArray.filter(item => item.tags.switch === filterData.switch[0])
+        }
+      }
+    }
+    
+    if (filterData.checkbox.length !== 0){
+      for (let i = 0 ; i < ads.length; i++){
+        localArray = localArray.filter(item => filterData.checkbox.some(checkItem => checkItem === item.tags.checkbox))
+      }
+    }
+
+    localArray = localArray.filter(item => item.price >= sliderRange[0] && item.price <= sliderRange[1])
+
+    if (filterData.switch.length > 1){
+      if (filterData.switch[1] !== 'any'){
+        for (let i = 0 ; i < ads.length; i++){
+          localArray = localArray.filter(item => item.tags.secondSwitch === filterData.switch[1])
+        }
+      }
+    }
+    if (filterData.dopeCheckbox.length !== 0){
+      for (let i = 0 ; i < ads.length; i++){
+        localArray = localArray.filter(item => filterData.dopeCheckbox.some(checkItem => checkItem === item.tags.dopeCheckbox))
+      }
+    }
+
+    if(filterData.select.length > 1){
+      localArray = localArray.filter(item => item.tags.secondSelect >= filterData.select[1])
+    }
+
+    setFilteredArr(localArray)
+    console.log(localArray)
+    
   }
 
     return (
       <div>
         <div style={{display: 'flex'}}>
           <CommonFilters />
-          <Content array={ads}/>
+          <Content array={filteredArr.length === 0 ? ads : filteredArr}/>
         </div>
         <ResultButton createArr={()=>createFilteredArr}/>
       </div>
